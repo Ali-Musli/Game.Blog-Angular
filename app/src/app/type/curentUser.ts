f09473e618a0ebffda40b0b7f3acbd1479048ba0
Ali <128837285+Ali-Musli@users.noreply.github.com>
2023-08-12 23:18:27 +0300

    Delete app directory

--- app/src/app/type/curentUser.ts
+++
@@ -1,4 +0,0 @@
-export interface CurentUser {
-    email: string,
-    _id: string
-}
--- app/src/app/type/game.ts
+++
@@ -1,7 +0,0 @@
-export interface Games {
-    "category": string,
-    "imageUrl": string,
-    "maxLevel": string, //number
-    "summary": string,
-    "title": string
-}
